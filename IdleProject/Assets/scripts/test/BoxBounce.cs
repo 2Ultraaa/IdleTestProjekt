7e21271bd47e56177bb9e89004b42458788deda2
2Ultraaa <34542928+2Ultraaa@users.noreply.github.com>
2018-01-05 18:00:51 +0100

    Neue Testszenen

---
+++ IdleProject/Assets/scripts/test/BoxBounce.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoxBounce : MonoBehaviour {
+
+    public Canvas canvas;
+    public GameObject box;
+    public GameObject button;
+    public float time;
+
+    RectTransform canvasTrans;
+    Vector3 startPos;
+    Vector3 endPos;
+    bool trigger;
+
+
+	void Start () {
+        //startposition ist immer eine screenlänger unterhalb des canvas, endposition immer in der mitte des canvas
+        canvasTrans = canvas.GetComponent<RectTransform>();
+        startPos = new Vector3(0, -1 * canvasTrans.rect.height, 0);
+        endPos = Vector3.zero;
+
+        //trigger toggle
+        trigger = false;
+
+        //zu beginn ist die box immer ausserhalb des canvas
+        box.SetActive(false);
+        box.GetComponent<RectTransform>().transform.localPosition = startPos;
+    }
+
+    IEnumerator Move(Vector3 sPos, Vector3 ePos, float seconds) {
+        //wenn außerhalb des canvas, während animation button nicht bedienbar
+        if (trigger == false) {
+            button.GetComponent<Button>().interactable = false;
+            float t = 0f;
+            while (t <= 1f)
+            {
+                t += Time.deltaTime / seconds;
+                box.transform.localPosition = Vector3.Lerp(sPos, ePos, Mathf.SmoothStep(0f, 1f, t));
+                yield return new WaitForFixedUpdate();
+            }
+            trigger = !trigger;
+            button.GetComponent<Button>().interactable = true;
+        }
+        //wenn innterhalb des canvas, während animation button nicht bedienbar
+        else if (trigger == true)
+        {
+            button.GetComponent<Button>().interactable = false;
+            float t = 0f;
+            while (t <= 1f)
+            {
+                t += Time.deltaTime / seconds;
+                box.transform.localPosition = Vector3.Lerp(ePos, sPos, Mathf.SmoothStep(0f, 1f, t));
+                yield return new WaitForFixedUpdate();
+            }
+            trigger = !trigger;
+            button.GetComponent<Button>().interactable = true;
+        }
+    }
+
+    //schiebe box nach oben
+    public void BoxCall () {
+        box.SetActive(true);
+        StartCoroutine(Move(startPos,endPos, time));
+    }
+}
---
+++ IdleProject/Assets/scripts/test/MenuButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour {
+
+    public GameObject box;
+
+    public void CallBox() {
+        box.GetComponent<BoxBounce>().BoxCall();
+    }
+}
