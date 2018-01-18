cff093e2d057fe3da0c6b454888129cc0411b3f5
2Ultraaa <34542928+2Ultraaa@users.noreply.github.com>
2018-01-18 17:59:43 +0100

    Testszene MapView
    Zooming und Scrolling

--- IdleProject/Assets/scripts/test/MapViewScroll.cs
+++ IdleProject/Assets/scripts/test/MapViewScroll.cs
@@ -1,16 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapViewScroll : MonoBehaviour {
 
     public float maxScale;
     public float minScale;
+    public float zoomSpeed;
+    public float resetDuration;
+
+    Canvas canvas;
+    float startScale;
     float currentScale;
 
+    private void Start() {
+        canvas = FindObjectOfType<Canvas>();
+        startScale = canvas.scaleFactor;
+    }
+
     void Update() {
-        //dividieren für besser interpolation
+        // Mausbedienung
         Zoom(Input.GetAxis("Mouse ScrollWheel"));
+
+        // If there are two touches on the device...
+        if (Input.touchCount == 2 && Input.GetTouch(0).phase == TouchPhase.Moved && Input.GetTouch(1).phase == TouchPhase.Moved)
+        {
+            GameObject.Find("ScrollMap").GetComponent<ScrollRect>().enabled = false;
+            // Store both touches.
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // Find the position in the previous frame of each touch.
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // Find the magnitude of the vector (the distance) between the touches in each frame.
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // Find the difference in the distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            if (currentScale != maxScale || currentScale != minScale)
+            {
+                Zoom(-1 * deltaMagnitudeDiff * zoomSpeed);
+            }
+        }
+
+        if ((Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended) || (Input.touchCount == 2 && Input.GetTouch(0).phase == TouchPhase.Ended && Input.GetTouch(1).phase == TouchPhase.Ended))
+        {
+            GameObject.Find("ScrollMap").GetComponent<ScrollRect>().enabled = true;
+        }
     }
 
     void Zoom(float increment) {
@@ -23,7 +64,7 @@ public class MapViewScroll : MonoBehaviour {
         {
             currentScale = minScale;
         }
-        this.gameObject.GetComponent<RectTransform>().transform.localScale = new Vector3(currentScale, currentScale, 1);
+        canvas.scaleFactor = currentScale;
     }
 
 }
